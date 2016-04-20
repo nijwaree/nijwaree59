9d2f1a46ae6db43a3180101d78747d9fd76c54fe
nijwaree <fonaugust@gmail.com>
2016-04-20 17:23:53 +0700

    1

--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/nijwaree_59/Detail_Activity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/nijwaree_59/Detail_Activity.java
@@ -1,7 +1,46 @@
 package ice_pbru.suksaovavimon.nijwaree.nijwaree_59;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by ICE on 20/4/2559.
  */
-public class Detail_Activity {
+public class Detail_Activity extends AppCompatActivity{
+
+    private TextView titleTextView, detailTextView;
+    private ImageView comImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // bind widget
+        bindWidget();
+
+        // Show view
+        showView();
+    }
+
+    private void showView() {
+        String strTitle = getIntent().getStringExtra("Title");
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.computerrrr);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+
+        titleTextView.setText(strTitle);
+        comImageView.setImageResource(intIcon);
+
+        String[] strDetail = getResources().getStringArray(R.array.com_detail);
+        detailTextView.setText(strDetail[intIndex]);
+    }
+
+    private void bindWidget() {
+        titleTextView = findViewById(R.id.textView4);
+
+
+
+    }
 }
